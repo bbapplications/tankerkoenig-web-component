25383b70319c8ca0ba5bd60562b005b3d7f14c27
Bruno Bührlen <info@bb-applications.de>
2019-08-07 11:03:08 +0200

    added vue slim tabs and axios

--- src/components/TkWebComponent.vue
+++ src/components/TkWebComponent.vue
@@ -2,15 +2,61 @@
     <div>
         <h1>TK Vue Web Component</h1>
         <div class="message">{{ msg }}</div>
+        <tabs>
+            <tab title="Benzin">
+                E10
+            </tab>
+            <tab title="Super">
+                E5
+            </tab>
+            <tab title="Diesel">
+                Diesel
+            </tab>
+        </tabs>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import { Tabs, Tab } from 'vue-slim-tabs';
     export default {
-        props: ['msg']
+        props: ['msg'],
+        components: {
+            Tabs, Tab
+        }
     }
 </script>
 <style>
+
+    .vue-tablist {
+        list-style: none;
+        display: flex;
+        padding-left: 0;
+        border-bottom: 1px solid #e2e2e2;
+    }
+
+    .vue-tab {
+        padding: 5px 10px;
+        cursor: pointer;
+        user-select: none;
+        border: 1px solid transparent;
+        border-bottom-color: #e2e2e2;
+        border-radius: 3px 3px 0 0;
+        background-color: white;
+        position: relative;
+        bottom: -1px;
+    }
+
+    .vue-tab[aria-selected="true"] {
+        border-color: #e2e2e2;
+        border-bottom-color: transparent;
+    }
+
+    .vue-tab[aria-disabled="true"] {
+        cursor: not-allowed;
+        color: #999;
+    }
+
     h1 {
         color: var(--headline-text-color, red);
     }
