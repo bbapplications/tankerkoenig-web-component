66e775c19c0f1dd40ba25a7b02f3a0b5c06b6475
Bruno Bührlen <info@bb-applications.de>
2019-08-14 13:36:20 +0200

    started added checks for stupid browsers

--- src/main.js
+++ src/main.js
@@ -2,6 +2,30 @@ import Vue from 'vue';
 import wrap from '@vue/web-component-wrapper';
 import TkWebComponent from './components/TkWebComponent';
 
-const WrappedElement = wrap(Vue, TkWebComponent);
 
-window.customElements.define('tk-web-component', WrappedElement);
+/*
+if (navigator.appName == 'Microsoft Internet Explorer'
+    || !!(navigator.userAgent.match(/Trident/)
+    || navigator.userAgent.match(/rv:11/))
+    || (/Edge\/\d./i.test(navigator.userAgent)) ) {
+    alert("your browser sucks!")
+}
+*/
+
+/*const WrappedElement = wrap(Vue, TkWebComponent);
+window.customElements.define('tk-web-component', WrappedElement);*/
+
+
+if(window.customElements) {
+    const WrappedElement = wrap(Vue, TkWebComponent);
+    window.customElements.define('tk-web-component', WrappedElement);
+} else {
+    var newDiv = document.createElement("div");
+    var newContent = document.createTextNode("Tankerkönig Web Komponente unterstützt keine veralteten Browser.");
+    newDiv.appendChild(newContent);
+
+    document.getElementById('tkwidget').appendChild(newDiv);
+
+}
+
+
